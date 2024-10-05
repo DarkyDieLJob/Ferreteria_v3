40564425490678d5954fe25fe42f2fd3fa815ada
DarkyDieLJob <diel.job@gmail.com>
2024-10-04 22:16:15 -0300

    fix(Celery): :bug: corregido error en ip de celery

--- core_config/celery.py
+++ core_config/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','core_config.settings')
 
-app = Celery(main='core_config', broker="amqp://guest:guest@172.17.0.3:5672//", )
+app = Celery(main='core_config', broker="amqp://guest:guest@0.0.0.0:5672//", )
 
 app.config_from_object('django.conf:settings',namespace='CELERY')
 
