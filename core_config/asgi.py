245f61bf424bba21b3c09a7c8898c6301766386b
DarkyDieLJob <diel.job@gmail.com>
2024-11-29 12:00:58 -0300

    fix(core): :hammer: Se implementa asgi

--- core_config/asgi.py
+++ core_config/asgi.py
@@ -13,4 +13,15 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_config.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from reactpy_django import REACTPY_WEBSOCKET_ROUTE  # noqa: E402
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter([REACTPY_WEBSOCKET_ROUTE]),
+    }
+)
