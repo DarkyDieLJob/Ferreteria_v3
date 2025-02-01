62322d40dbe3254f3f67c0408be1c60410b521c6
DarkyDieLJob <diel.job@gmail.com>
2025-02-01 10:45:34 -0300

    fix(app_pedido): :bug: Se corrigieron urls
    Se implementa reestructuracion leve.

--- pedido/urls.py
+++ pedido/urls.py
@@ -1,11 +1,30 @@
 from django.urls import path
-from .views import ItemAutocomplete, ListarPedidosView, NuevoStockView, EditarPedidoView, actualizar_llego, actualizar_cantidad
+from .views import ItemAutocomplete, ListarPedidosView
+from .views import NuevoStockView, EditarPedidoView
+from .views import actualizar_llego, actualizar_cantidad
+from .views import HomeView, ListarArticulosFaltantesView
+
+name = 'pedido'
 
 urlpatterns = [
-    path('nuevo_pedido/', NuevoStockView.as_view(), name='NuevoStockView'), 
+    path('home/', HomeView.as_view(), name='home'), 
+    
+    path('listar_faltantes/<int:proveedor_id>',
+         ListarArticulosFaltantesView.as_view(),
+         name='listar-faltantes'
+         ), 
+    path('editar_pedido_por_proveedor/<int:proveedor_id>',
+         EditarPedidoView.as_view(),
+         name='editar-pedido-por-proveedor'
+         ),
+    
+    path('pedir_nuevo_articulo/', NuevoStockView.as_view(), name='NuevoStockView'), 
+    
     path('item_autocomplete/', ItemAutocomplete.as_view(), name='item-autocomplete'),
     path('listar_pedidos/', ListarPedidosView.as_view(), name='listar-pedidos'),
     path('editar_pedido/<int:pedido_id>', EditarPedidoView.as_view(), name='editar-pedido'),
+    
     path('actualizar_llego/<int:articulo_id>', actualizar_llego, name='actualizar-llego'),
     path('actualizar_cantidad/<int:articulo_id>', actualizar_cantidad, name='actualizar-cantidad'),
+    
 ]
