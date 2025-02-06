a14b99ddab7b0ee1d0cd7a88978c5737e1a1e063
DarkyDieLJob <diel.job@gmail.com>
2025-02-06 15:39:43 -0300

    fix(app_pedido): :hammer: Se implemento la modificacion de cantidades en el pedido

--- pedido/views.py
+++ pedido/views.py
@@ -260,7 +260,25 @@ def actualizar_cantidad(request, articulo_id):
     print("Actualizando cantidad de articulo", articulo_id)
     articulo_pedido = ArticuloPedido.objects.get(id=articulo_id)
     data = json.loads(request.body)
-    articulo_pedido.cantidad = data.get('cantidad')
+    
+    cantidad = float(data.get('cantidad'))
+    
+    if articulo_pedido.cantidad > cantidad:
+        articulo_faltante,_ = Lista_Pedidos.objects.get_or_create(
+            proveedor=articulo_pedido.proveedor, 
+            item=articulo_pedido.item
+            )
+        articulo_faltante.cantidad = articulo_faltante.cantidad - (cantidad - articulo_pedido.cantidad)
+        articulo_faltante.save()
+        
+    elif articulo_pedido.cantidad < cantidad:
+        articulo_faltante,_ = Lista_Pedidos.objects.get_or_create(
+            proveedor=articulo_pedido.proveedor, 
+            item=articulo_pedido.item
+            )
+        articulo_faltante.cantidad = articulo_faltante.cantidad + (articulo_pedido.cantidad - cantidad)
+        articulo_faltante.save()
+    articulo_pedido.cantidad = cantidad
     articulo_pedido.save()
     return JsonResponse({'status': 'ok'})
 
