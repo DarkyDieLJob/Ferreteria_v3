7ccf0d5e21613312e9bbfe3fb9f46b23669f80a1
DarkyDieLJob <diel.job@gmail.com>
2025-02-15 13:59:51 -0300

    refactor(app_pedido): :recycle: Se reordeno el listado de urls. de cada a reordenar los templates y las vistas.

--- pedido/urls.py
+++ pedido/urls.py
@@ -13,39 +13,117 @@ app_name = 'pedido'
 name = 'pedido'
 
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home'), 
-    
-    path('listar_faltantes/<int:proveedor_id>',
-         ListarArticulosFaltantesView.as_view(),
-         name='listar-faltantes'
+     #Home vista de inicio
+     path('home/',
+         HomeView.as_view(),
+         name='home'
          ), 
-    path('editar_pedido_por_proveedor/<int:proveedor_id>',
-         EditarPedidoView.as_view(),
-         name='editar-pedido-por-proveedor'
-         ),
-    path('nuevo_pedido/<int:proveedor_id>',
+     
+     path('nuevo_pedido/<int:proveedor_id>',
          nuevo_pedido,
          name='nuevo-pedido'
          ),
-    
-    # En el siguiente path, se encuentra un buscador de artículos en tiempo real.
-    path('pedir_nuevo_articulo/', NuevoStockView.as_view(), name='NuevoStockView'), 
-    
-    path('item_autocomplete/', ItemAutocomplete.as_view(), name='item-autocomplete'),
-    path('listar_pedidos/', ListarPedidosView.as_view(), name='listar-pedidos'),
-    path('editar_pedido/<int:pedido_id>', EditarPedidoView.as_view(), name='editar-pedido'),
-    
-    path('actualizar_llego/<int:articulo_id>', actualizar_llego, name='actualizar-llego'),
-    path('actualizar_cantidad/<int:articulo_id>', actualizar_cantidad, name='actualizar-cantidad'),
-    path('agregar_al_pedido/', agregar_al_pedido, name='agregar-al-pedido'),
-    path('cancelar_articulo_pedido/', cancelar_articulo_pedido, name='cancelar-articulo-pedido'),
-    path('enviar_pedido/', enviar_pedido, name='enviar-pedido'),
-    path('controlar_pedido/<int:pedido_id>', ControlarPedidoView.as_view(), name='controlar-pedido'),
-    path('detalle_pedido/<int:pedido_id>', DetallePedidoView.as_view(), name='detalle-pedido'),
-    path('agregar_al_stock/', agregar_al_stock, name='agregar-al-stock'),
-    path('marcar_controlado/', ControlarPedidoView.as_view(), name='marcar-controlado'),
-    path('agregar_devolucion/', agregar_devolucion, name='agregar-devolucion'),
-    path('listar_devoluciones/', ListarDevolucionesView.as_view(), name='listar-devoluciones'),
-    path('eliminar_devolucion/', ListarDevolucionesView.as_view(), name='eliminar-devolucion'),
+     
+     #Buscar articulos
+     path('item_autocomplete/',
+         ItemAutocomplete.as_view(),
+         name='item-autocomplete'
+         ),
+     
+     path('agregar_al_stock/',
+         agregar_al_stock,
+         name='agregar-al-stock'
+         ),
+     
+     #Listar articulos faltantes
+     path('listar_faltantes/<int:proveedor_id>',
+         ListarArticulosFaltantesView.as_view(),
+         name='listar-faltantes'
+         ), 
+     
+     #Editar pedido por proveedor
+     path('editar_pedido_por_proveedor/<int:proveedor_id>',
+         EditarPedidoView.as_view(),
+         name='editar-pedido-por-proveedor'
+         ),
+     
+     path('editar_pedido/<int:pedido_id>',
+         EditarPedidoView.as_view(),
+         name='editar-pedido'
+         ),
+     
+     path('actualizar_cantidad/<int:articulo_id>',
+         actualizar_cantidad,
+         name='actualizar-cantidad'
+         ),
+     
+     path('agregar_al_pedido/',
+         agregar_al_pedido,
+         name='agregar-al-pedido'
+         ),
+     
+     path('cancelar_articulo_pedido/',
+         cancelar_articulo_pedido,
+         name='cancelar-articulo-pedido'
+         ),
+     
+     path('enviar_pedido/',
+         enviar_pedido,
+         name='enviar-pedido'
+         ),
+     
+     #Detalle de pedido por proveedor
+     path('detalle_pedido/<int:pedido_id>',
+         DetallePedidoView.as_view(),
+         name='detalle-pedido'
+         ),
+     
+     #Controlar pedido por proveedor
+     path('controlar_pedido/<int:pedido_id>',
+         ControlarPedidoView.as_view(),
+         name='controlar-pedido'
+         ),
+     
+     path('agregar_devolucion/',
+         agregar_devolucion,
+         name='agregar-devolucion'
+         ),
+     
+     path('actualizar_llego/<int:articulo_id>',
+         actualizar_llego,
+         name='actualizar-llego'
+         ),
+     
+     path('marcar_controlado/',
+         ControlarPedidoView.as_view(),
+         name='marcar-controlado'
+         ),
+     
+     #Listar devoluciones
+     path('listar_devoluciones/',
+         ListarDevolucionesView.as_view(),
+         name='listar-devoluciones'
+         ),
+     
+     path('actualizar_cantidad/<int:articulo_id>',
+         actualizar_cantidad,
+         name='actualizar-cantidad'
+         ),
+     
+     path('pedir_nuevo_articulo/', 
+         NuevoStockView.as_view(), 
+         name='NuevoStockView'
+         ), 
+     
+     path('listar_pedidos/',
+         ListarPedidosView.as_view(),
+         name='listar-pedidos'
+         ),
+     
+     path('eliminar_devolucion/',
+         ListarDevolucionesView.as_view(),
+         name='eliminar-devolucion'
+         ),
+     
 
 ]
