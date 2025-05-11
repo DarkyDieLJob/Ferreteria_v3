caaa4c3f12e5ffb41124c396ca8c380efc602eb8
Matías Leandro López <diel.job@gmail.com>
2025-05-11 00:21:40 -0300

    fix(Actualizador): :zap: Se implementa una vista para actualizar en 1 minuto

--- actualizador/urls.py
+++ actualizador/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Actualizar, Reckup
+from .views import Actualizar, Reckup, ActualizarAhora
 
 urlpatterns = [
     path('actualizar/', Actualizar.as_view(), name='actualizar'),
     path('reckup/', Reckup.as_view(), name='reckup'),
+    path('actualizar/ahora/', ActualizarAhora.as_view(), name='actualizar_ahora'),
 ]
